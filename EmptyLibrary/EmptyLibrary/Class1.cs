13963c4feb389399351498d9b48e89cdacdf1392
Robert Wagner <robert@wagner.id.au>
2017-03-10 16:07:58 +1000

    First

---
+++ EmptyLibrary/EmptyLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmptyLibrary
+{
+    public class Class1
+    {
+    }
+}
